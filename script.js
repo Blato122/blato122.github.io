3f30fe6136b5f2b71e0a0b51e99d382cb3bae05a
blato <blator02@gmail.com>
2024-03-27 19:15:38 +0100

    some new cool stuff

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-
-
 const base_url = 'https://raw.githubusercontent.com/blato122/mont-blanc-cam/main/gouter/';
   
 const today = new Date(); // current date, can't go past that
-const init = new Date('March 26, 2024 20:00:00'); // date of starting the program, can't go earlier than that
+const init = new Date('March 27, 2024 8:00:00'); // date of starting the program, can't go earlier than that
 
-let date = new Date();
-
-let current_year = date.getFullYear();
-let current_month = date.getMonth() + 1; // months are 0-indexed
-let current_day = date.getDate();
-let current_hour = date.getHours();
+let current_year = today.getFullYear();
+let current_month = today.getMonth() + 1; // months are 0-indexed
+let current_day = today.getDate();
+let current_hour = today.getHours();
 
 let img_element = document.getElementById('gouter-photo');
-let prev_hour_button = document.getElementById('gouter-prev-hour');
-let next_hour_button = document.getElementById('gouter-next-hour');
-// zamiast tego to suwak z boku z godzinami, co przeskakuje o 1!
-// i wtedy nie trzeba checka czy nie wychodzi się poza 7 lub 22!
 
 function update_photo() {
     let hour_str = (current_hour >= 10) ? current_hour : ("0" + current_hour);
-    let img_url = `${base_url}${current_year}/${current_month}/${current_day}/${current_hour}.jpg`;
-    console.log(img_url)
+    let img_url = `${base_url}${current_year}/${current_month}/${current_day}/${hour_str}.jpg`;
+    console.log(img_url);
     img_element.src = img_url;
 }
 
@@ -29,52 +21,44 @@ function update_date(hours=0, days=0, months=0, years=0) {
     current_year = current_year + years;
     current_month = current_month + months;
     current_day = current_day + days;
-    current_hour = current_hour + hours; // redundant all od this
-    update_photo();
+    current_hour = current_hour + hours;
+
+    let current_date = new Date(current_year, current_month, current_day, current_hour)
+    if (current_date >= init && current_date <= today) {
+        update_photo();
+    }
 }
 
-prev_hour_button.addEventListener('click', () => {
-    update_date(-1);
+// prev_month_button.addEventListener('click', () => {
+//     update_date(0, 0, -1, 0);
+// });
+
+// next_month_button.addEventListener('click', () => { 
+//     update_date(0, 0, 1, 0);
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("gouter-time-slider").value = today.getHours();
+    document.getElementById("gouter-hour").innerText = today.getHours();
 });
 
-next_hour_button.addEventListener('click', () => { // ale cool! można godzinami przechodzić między dniami!!! ale zmienić że jak się zmniejsza na 7
-// czy tam 6, to przenosi na ostanią godzinę poprzedniego dnia czyli 21 lib 22, nwm
-// całkiem ez to 
+img_element.addEventListener("error", () => {
+    img_element.src = 'image-not-found.png';
+});
 
-// no i blokować dalsze przesuwanie jak już nie ma zdjęć!! żeby nie wychodzić coraz dalej za zakres!
-// i dać przycisk powrotu do bieżącej daty :))))))
-    update_date(1);
+let slider = document.getElementById("gouter-time-slider");
+let hourDisplay = document.getElementById("gouter-hour");
+
+// Update the display value when slider value changes
+slider.addEventListener("input", function() {
+  hourDisplay.innerText = slider.value;
+  prev_hour = current_hour;
+  current_hour = Number(slider.value);
+  update_date(current_hour - prev_hour);
 });
 
 // Initial update
 update_photo();
-
 // time zone?
-
-// into 2 files maybe idk 
-
-// JavaScript code to set the value of the range input programmatically
-document.addEventListener("DOMContentLoaded", function() {
-    // Set the value to 10
-    document.getElementById("gouter-time-slider").value = today.getHours();
-    // Update the display value
-    document.getElementById("hour").innerText = today.getHours();
-});
-
-// Get the slider element
-let slider = document.getElementById("gouter-time-slider");
-// Get the display element
-let hourDisplay = document.getElementById("hour");
-
-// Update the display value when slider value changes
-slider.addEventListener("input", function() {
-  // Update the display value
-  hourDisplay.innerText = slider.value;
-  prev_hour = current_hour
-  current_hour = Number(slider.value)
-  update_date(current_hour - prev_hour) // clean thta up???
-});
-
 // split into 2 files???
-
 // usunac 26 marca bo nie ma wszystkich zdjec
