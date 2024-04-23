206903d06422bcf80dade7eaa4d2cbcc5bdbd611
blato <blator02@gmail.com>
2024-04-23 17:21:45 +0200

    modal working?

--- modal.js
+++ modal.js
@@ -1,14 +1,19 @@
+// https://www.w3schools.com/js/js_loop_forof.asp
+// https://www.w3schools.com/howto/howto_css_modal_images.asp
+
 window.addEventListener('load', () => {
     // Get the modal
     let modal = document.getElementById("modal-full-img");
     // Get the <span> element that closes the modal - why not by id??
     let close_modal = document.getElementsByClassName("close-modal")[0];
     // Get the button that opens the modal
-    let button = document.getElementsByClassName("modal-button");
+    let buttons = document.getElementsByClassName("modal-button");
 
-    // When the user clicks the button, open the modal 
-    button.onclick = () => {
-        modal.style.display = "block";
+    // When the user clicks a photo, open the modal 
+    for (let button of buttons) {
+        button.addEventListener('click', () => {
+            modal.style.display = "block";
+        });
     }
 
     // When the user clicks on <span> (x), close the modal
