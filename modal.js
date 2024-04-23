ddfae6524faced4073c89bc2b56a3a809234e68f
blato <blator02@gmail.com>
2024-04-23 17:18:40 +0200

    modal change 2

--- modal.js
+++ modal.js
@@ -1,23 +1,25 @@
-// Get the modal
-let modal = document.getElementById("modal-full-img");
-// Get the <span> element that closes the modal - why not by id??
-let close_modal = document.getElementsByClassName("close-modal")[0];
-// Get the button that opens the modal
-let button = document.getElementsByClassName("modal-button");
+window.addEventListener('load', () => {
+    // Get the modal
+    let modal = document.getElementById("modal-full-img");
+    // Get the <span> element that closes the modal - why not by id??
+    let close_modal = document.getElementsByClassName("close-modal")[0];
+    // Get the button that opens the modal
+    let button = document.getElementsByClassName("modal-button");
 
-// When the user clicks the button, open the modal 
-button.onclick = () => {
-    modal.style.display = "block";
-}
+    // When the user clicks the button, open the modal 
+    button.onclick = () => {
+        modal.style.display = "block";
+    }
 
-// When the user clicks on <span> (x), close the modal
-close_modal.onclick = () => {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    close_modal.onclick = () => {
         modal.style.display = "none";
     }
-}
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
