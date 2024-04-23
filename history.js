e70e8acf74258cd422fbb5ce62849d42838eca74
blato <blator02@gmail.com>
2024-04-23 18:22:34 +0200

    history refresh 4

--- history.js
+++ history.js
@@ -1,25 +1,28 @@
 // Function to dump HTML content into localStorage
-function dumpHTMLToLocalStorage() {
+function dump_html() {
     const htmlContent = document.documentElement.outerHTML;
-    localStorage.setItem('savedHTML', htmlContent);
+    sessionStorage.setItem('savedHTML', htmlContent);
+    sessionStorage.setItem("is_reloaded", "true");
 }
 
 // Function to load HTML content from localStorage
-function loadHTMLFromLocalStorage() {
-    const savedHTML = localStorage.getItem('savedHTML');
+// czy session może?
+function load_html() {
+    const savedHTML = sessionStorage.getItem('savedHTML');
     if (savedHTML) {
         document.open();
         document.write(savedHTML);
         document.close();
     }
+    sessionStorage.setItem("is_reloaded", "false");
 }
 
 // Dump HTML content into localStorage when the page unloads
-window.addEventListener('unload', dumpHTMLToLocalStorage);
+window.addEventListener('unload', dump_html);
 
 // Load HTML content from localStorage when the page loads
 // window.addEventListener('load', loadHTMLFromLocalStorage);
 
-if (sessionStorage.getItem("is_reloaded")) {
-    loadHTMLFromLocalStorage();
+if (sessionStorage.getItem("is_reloaded") === "true") {
+    load_html();
 }
